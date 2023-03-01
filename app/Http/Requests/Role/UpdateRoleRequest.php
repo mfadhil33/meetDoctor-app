d1a6e3bae8baffb50d35ff6d6ac0210911da554f
Mhd Fadhil Arya Putra <fadhilgaming522@gmail.com>
2023-03-01 11:38:28 +0300

    request update finally

--- app/Http/Requests/Role/UpdateRoleRequest.php
+++ app/Http/Requests/Role/UpdateRoleRequest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Symfony\Component\HttpFoundation\Response;
 // this rule only  at update  request
 use Illuminate\Validation\Rule;
+
 class UpdateRoleRequest extends FormRequest
 {
     /**
@@ -27,7 +28,7 @@ class UpdateRoleRequest extends FormRequest
     {
         return [
             'title' => [
-                'required', 'string', 'max:255'
+                'required', 'string', 'max:255', Rule::unique('role')->ignore($this->role)
             ]
         ];
     }
